5b182fbc45a085c4d29df7fe12fef3f05848eb42
oshaneeperz <oshanee.20223145@iit.ac.lk>
2023-12-01 11:20:35 +0530

    Initial commit

---
+++ react-sltUI/src/App.jsx
@@ -0,0 +1,38 @@
+
+
+function App() {
+ 
+  return (
+    <>
+    <div>
+      <img src="https://sltds.lk/wp-content/uploads/2021/05/cropped-logo1.png" className="sltLogo"/>
+    </div>
+    <h3 className="top_topic">SLT Digital Services</h3>
+    <h5 className="top_topic">INTRANET</h5>
+    <div className="square">
+      <h4><b>Approval for Pre-Overtime Requests</b></h4>
+      <h6>Please Login to the Intranet and Approve pending Requests</h6>
+    </div>
+    <marquee>
+      <div className="image-marquee">
+        <img src="https://img.freepik.com/free-photo/abstract-background-paint-illustration-ink-backdrop-drop-creativity-generated-by-ai_188544-15660.jpg?w=900&t=st=1701406732~exp=1701407332~hmac=7221b087ad5eb4705a9f88eeb2301334e0a048707d7b6ac9a054228cfb358ec3" id="img1"/>
+        <img src="https://img.freepik.com/free-photo/woman-sitting-desk-medium-shot_23-2149713671.jpg?w=996&t=st=1701405728~exp=1701406328~hmac=988f4a741f21dd1fe50f1eab752829e696f9172281a71c3c6b3f811306b8c955" id="img2"/>
+        <img src="https://img.freepik.com/free-photo/study-knowledge-academics-institute-graphic_53876-13849.jpg?w=826&t=st=1701404978~exp=1701405578~hmac=476a4a474cf2408f2b9e6bfbb094d5d4e7a8e9f4adb4a278e7433201c958eac1" id="img3" />
+        <img src="https://img.freepik.com/free-photo/art-studio-table-full-materials_23-2148002505.jpg?w=996&t=st=1701352999~exp=1701353599~hmac=5d46c8c3b4fd18c721588c5ed05b9601c7473337e385275ad0ef8e1449bc7ed3" id="img4" />
+        <img src="https://img.freepik.com/free-photo/bunch-flowers-leaves-table_1304-4187.jpg?w=996&t=st=1701407296~exp=1701407896~hmac=d9b68076ff7e4248d93d91d77f70c4a09d59b1eba5f922d755f1e587b2b90fb8" id="img5"/>
+        <img src="https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010125.jpg?t=st=1701407393~exp=1701407993~hmac=91772e293b3d0bc44262238c2cd29a6a7fdf310ccf7778655be04b70332a8a5a"id="img6"/>
+        <img src="https://img.freepik.com/premium-photo/text-source-data-jigsaw-puzzle-with-calculator-green-background_406607-3850.jpg?w=826"id="img7"/>
+        <img src="https://img.freepik.com/free-photo/group-five-african-college-students-spending-time-together-campus-university-yard-black-afro-friends-studying-education-theme_627829-6007.jpg?w=996&t=st=1701407453~exp=1701408053~hmac=09849a23fc14caf3d35cc447ed49617633145eaaf604b504e505d07eac1b06f1"id="img8"/>
+        <img src="https://img.freepik.com/free-photo/backpacker-standing-sunrise-viewpoint-ja-bo-village-mae-hong-son-province-thailand_335224-1356.jpg?w=996&t=st=1701407519~exp=1701408119~hmac=ad73f22e30a702aaef002964ff71e9db36453d2d10cdc44e1f6b4c44e38d1e1f"id="img9"/>
+        <img src="https://img.freepik.com/free-photo/indulgent-chocolate-dessert-with-fresh-fruit-decoration-generative-ai_188544-9688.jpg?w=1060&t=st=1701407579~exp=1701408179~hmac=215ce4b6663fbb0fe6069e42953e5969d50959f59ccd83334dc7653c637dd369"id="img10"/>
+      
+    
+      </div>
+      </marquee>
+    
+    </>
+  )
+  
+  }
+
+export default App
